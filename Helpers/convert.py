1928c5f6a2ba9e91cb5de9c603fb51da2bc04399
Dhruvik <dhruvik.barvaliya.blackwolve@gmail.com>
2024-05-03 12:25:50 +0530

    test

--- Helpers/convert.py
+++ Helpers/convert.py
@@ -3,9 +3,25 @@
 
 # Import the convert method from the
 # docx2pdf module
+print("aaaaaaaaaaaaaa")
 from docx2pdf import convert
 
 # convert(r"C:\Users\EV\Desktop\Property\modified.docx", r"C:\Users\EV\Desktop\Property\output.pdf")
+import subprocess
+
+print("bbbbbbbbbbbbbbbbbbb")
+
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+# Usage example
+package_name = "your_package_name"
+install("docx2pdf")
+
+print("ccccccccccc")
+
 
 
 def convert_docx_to_pdf(input_file, output_file):
@@ -18,3 +34,4 @@ if __name__ == "__main__":
     arg1 = sys.argv[1]
     arg2 = sys.argv[2]
     result = convert_docx_to_pdf(arg1, arg2)
+print("dddddddddddddddddddddddddd")
